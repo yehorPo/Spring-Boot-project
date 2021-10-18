ce348d2e7102d2737f920b9fdb7177d521f53f11
yehorPo <yehor.podolsk@gmail.com>
2021-10-18 13:41:15 +0300

    added personized courses shower page with grid

---
+++ front/js/grid/getMyGrid.js
@@ -0,0 +1,7 @@
+$(function(){
+    let data = [
+        {name: "Monday", t_name: "Monday", time: "Monday", link: "Monday"},
+        {name: "Tuesday", t_name: "Tuesday", time: "Tuesday", link: "Tuesday"}
+    ];
+    createGrid(data);
+})
